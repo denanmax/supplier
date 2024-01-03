17d46ed37b4b0542255824dfc03d4cb2edbd1374
denanmax <deshis93@gmail.com>
2024-01-03 14:50:43 +0300

    perms added

--- network/permissions.py
+++ network/permissions.py
@@ -2,6 +2,6 @@ from rest_framework.permissions import BasePermission
 
 
 class IsActiveStuff(BasePermission):
-
+    """Пермишн для проверки активного юзера и персонала"""
     def has_permission(self, request, view):
         return request.user.is_active and request.user.is_staff
