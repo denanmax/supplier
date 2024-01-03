92291e1131dc9e904fcdb0e884e7198ec866ce05
denanmax <deshis93@gmail.com>
2024-01-03 12:14:21 +0300

    base view/tested

--- network/views.py
+++ network/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import django_filters
+from rest_framework import generics, viewsets
+from network.models import Manufacturer, Supplier, Delivery
+from network.serializers import ManufacturerSerializer, DeliverySerializer
 
-# Create your views here.
+
+class ManufacturerAPIView(generics.ListAPIView):
+    queryset = Manufacturer.objects.all()
+    serializer_class = ManufacturerSerializer
+
+
+class ManufacturerCreateAPIView(generics.CreateAPIView):
+    serializer_class = ManufacturerSerializer
+
+
+class ManufacturerRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = ManufacturerSerializer
+    queryset = Manufacturer.objects.all()
+
+
+class ManufacturerUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = ManufacturerSerializer
+    queryset = Manufacturer.objects.all()
+
+
+class ManufacturerDestroyAPIView(generics.DestroyAPIView):
+    queryset = Manufacturer.objects.all()
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    serializer_class = DeliverySerializer
+    queryset = Delivery.objects.all()
