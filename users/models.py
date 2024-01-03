454fd37d2d4ced1a9e8f0ec9a4846cd3abc1d526
denanmax <deshis93@gmail.com>
2024-01-03 14:52:16 +0300

    cleaned up

--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
+    """Модель создания пользователей"""
 
     username = None
 
-    email = models.EmailField(verbose_name='почта', unique=True)
-    name = models.CharField(max_length=50, verbose_name='имя', null=True, blank=True)
-    phone = models.CharField(max_length=20, verbose_name='телефон', null=True, blank=True)
+    email = models.EmailField(verbose_name='Почта', unique=True)
+    name = models.CharField(max_length=50, verbose_name='Имя', null=True, blank=True)
+    phone = models.CharField(max_length=20, verbose_name='Телефон', null=True, blank=True)
     is_staff = models.BooleanField(default=False, verbose_name='Сотрудник')
     is_active = models.BooleanField(default=False, verbose_name='Активен')
 
