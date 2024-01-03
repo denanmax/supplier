8987c339b8fc3b1105c23ee3ecd4f91c86b76a71
denanmax <deshis93@gmail.com>
2024-01-03 14:51:35 +0300

    delivery api view created to avoid debt update

--- network/views.py
+++ network/views.py
@@ -1,13 +1,40 @@
-import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, viewsets
+from rest_framework.filters import OrderingFilter
+
 from network.models import Manufacturer, Supplier, Product, Delivery
+from network.paginators import ManufacturerPaginator
 from network.permissions import IsActiveStuff
 from network.serializers import ManufacturerSerializer, SupplierSerializer, ProductSerializer, DeliverySerializer
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    """Вьюсет для Сапплаера с фильтрацией по стране"""
+    serializer_class = SupplierSerializer
+    queryset = Supplier.objects.all()
+    permission_classes = [IsActiveStuff]
+    pagination_class = ManufacturerPaginator
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('country',)
+    ordering_fields = ['country']
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    permission_classes = [IsActiveStuff]
+
+
+class DeliveryAPIView(generics.ListAPIView):
+    queryset = Delivery.objects.all()
+    serializer_class = DeliverySerializer
+    permission_classes = [IsActiveStuff]
 
 
 class ManufacturerAPIView(generics.ListAPIView):
     queryset = Manufacturer.objects.all()
     serializer_class = ManufacturerSerializer
+    pagination_class = ManufacturerPaginator
     permission_classes = [IsActiveStuff]
 
 
@@ -30,22 +57,4 @@ class ManufacturerUpdateAPIView(generics.UpdateAPIView):
 
 class ManufacturerDestroyAPIView(generics.DestroyAPIView):
     queryset = Manufacturer.objects.all()
-    permission_classes = [IsActiveStuff]
-
-
-class SupplierViewSet(viewsets.ModelViewSet):
-    serializer_class = SupplierSerializer
-    queryset = Supplier.objects.all()
-    permission_classes = [IsActiveStuff]
-
-
-class ProductViewSet(viewsets.ModelViewSet):
-    serializer_class = ProductSerializer
-    queryset = Product.objects.all()
-    permission_classes = [IsActiveStuff]
-
-
-class DeliveryViewSet(viewsets.ModelViewSet):
-    serializer_class = DeliverySerializer
-    queryset = Delivery.objects.all()
     permission_classes = [IsActiveStuff]
